ecda6d549cc7364edbc10f973659113a3ca218af
Guido Sabik <guido.sabik@gmail.com>
2022-03-03 19:50:53 -0300

    updates

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-ScrollReveal().reveal('aboutme-left');
+const sr = ScrollReveal({
+    duration:2000,
+})
+
+/*Titles*/ 
+sr.reveal('.aboutme-title, .skills-title, .projects-title, .contact-title',{});
+
+/*Presentation*/
+sr.reveal('.hero-title',{
+    origin: 'left',
+    distance: '80px'
+});
+
+sr.reveal('.hero-l',{
+    delay:600
+});
+
+/*Aboute Me*/
+sr.reveal('.aboutme-left',{
+    origin:'bottom',
+    distance:'100px',
+    delay:200
+});
+
+sr.reveal('.aboutme-right',{
+    origin:'left',
+    distance:'50px',
+    delay: 500
+});
+
+/*Skills*/
+
+sr.reveal('.skills-description',{
+    delay:500
+});
+
+sr.reveal('.skills-list-left',{
+    delay:700
+});
+
+sr.reveal('.skills-list-right',{
+    delay:1000
+});
+
+/*Projects*/
+sr.reveal('.project-description',{
+    origin: 'left',
+    distance:'50px'
+});
+
+sr.reveal('.project-img',{
+    delay:500
+});
+
+/*Contact */
+sr.reveal('form',{
+    delay:500,
+});
+
+sr.reveal('.contact-items',{
+    delay:900
+});
